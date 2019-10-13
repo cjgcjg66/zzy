a633d4ed26728e5cbc845bcfa85669fa21b4045b
github2570170721 <githubcjg@.com>
2019-10-13 02:09:57 -0400

    第一次。。。

---
+++ src/hello.java
@@ -0,0 +1,7 @@
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("sfsdf");
+        System.out.println("dsggdsg");
+        System.out.println("\n");
+    }
+}
