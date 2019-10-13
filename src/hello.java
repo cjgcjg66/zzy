19edcfcc2419912f3918b2ab02e469bf5c1ea907
github2570170721 <githubcjg@.com>
2019-10-13 02:13:03 -0400

    第三次。。。

--- src/hello.java
+++ src/hello.java
@@ -2,7 +2,8 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("sfsdf");
         System.out.println("dsggdsg");
-        System.out.println("asdfgtyu");
+        System.out.println("utrhgrfesa");
+
         System.out.println("\n");
     }
 }
