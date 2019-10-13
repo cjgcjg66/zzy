f7f31807c6d371fc60095639f62a8a4079d4d9c3
cjgcjg66 <cjgcjg66@outlook.com>
2019-10-13 02:35:04 -0400

    第四次。。。

--- src/hello.java
+++ src/hello.java
@@ -1,9 +1,8 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("sfsdf");
-        System.out.println("dsggdsg");
+        System.out.println("sdf");
         System.out.println("utrhgrfesa");
-
         System.out.println("\n");
     }
 }
